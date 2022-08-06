72ba8fc296510ebaa035e88a5872c33d1bbefa63
mohamednajiub <mohamednajiub@gmail.com>
2022-08-06 18:35:42 +0200

    feat: convert 404.html to 404.pug

--- routes/admin.js
+++ routes/admin.js
@@ -2,10 +2,6 @@ const express = require("express");
 
 const router = express.Router();
 
-const path = require("path");
-
-const rootDir = require("../util/path");
-
 const products = [];
 
 router.get("/add-product", (req, res, next) => {
--- routes/shop.js
+++ routes/shop.js
@@ -2,8 +2,6 @@ const express = require("express");
 
 const router = express.Router();
 
-const path = require("path");
-const rootDir = require("../util/path");
 const adminData = require("./admin");
 
 router.get("/", (req, res, next) => {
