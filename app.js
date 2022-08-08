38517484af6723b5eef06ffd1e1ed9a0b52111aa
mohamednajiub <mohamednajiub@gmail.com>
2022-08-08 21:43:21 +0200

    feat: Replace handlebars with ejs

--- app.js
+++ app.js
@@ -3,19 +3,12 @@ const bodyParser = require("body-parser");
 
 const path = require("path");
 
-const expressHBS = require("express-handlebars");
-
 const adminData = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
 const app = express();
 
-var handlebars = expressHBS.create({
-  extname: ".hbs",
-  // helpers: handlebarsHelpers,
-});
-app.engine(".hbs", handlebars.engine);
-app.set("view engine", ".hbs");
+app.set("view engine", "ejs");
 
 app.set("views", "views");
 
@@ -28,6 +21,7 @@ app.use(shopRoutes);
 app.use((req, res, next) => {
   res.status(404).render("404", {
     docTitle: "Page Not Found",
+    path: "404",
   });
 });
 
