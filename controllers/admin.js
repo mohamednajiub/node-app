f4b1e2064d7c7d467adfe088d1445d45927493c1
mohamednajiub <mohamednajiub@gmail.com>
2022-08-10 22:17:32 +0200

    feat: Add edit product functionality

--- controllers/admin.js
+++ controllers/admin.js
@@ -35,7 +35,18 @@ exports.postAddProduct = (req, res, next) => {
     const imageUrl = req.body.imageUrl;
     const price = req.body.price;
     const description = req.body.description;
-    const product = new Product(title, imageUrl, description, price);
+    const product = new Product(null, title, imageUrl, description, price);
+    product.save();
+    res.redirect('/admin/products');
+};
+
+exports.postEditProduct = (req, res, next) => {
+    const id = req.body.id;
+    const title = req.body.title;
+    const imageUrl = req.body.imageUrl;
+    const price = req.body.price;
+    const description = req.body.description;
+    const product = new Product(id, title, imageUrl, description, price);
     product.save();
     res.redirect('/');
 };
