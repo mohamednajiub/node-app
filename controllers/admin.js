197442bc0b393402a5accc9e74417807b161188f
mohamednajiub <mohamednajiub@gmail.com>
2022-08-10 21:07:47 +0200

    feat: Add Edit Product form and render data in it

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,10 +1,33 @@
 const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
-    res.render('admin/add-product', {
+    res.render('admin/edit-product', {
         pageTitle: 'Add Product',
         path: '/admin/add-product',
+        editing: false
     });
+};
+
+exports.getEditProduct = (req, res, next) => {
+    const editMode = req.query.edit
+
+    if (!editMode) {
+        return res.redirect('/')
+    }
+
+    const prodId = req.params.productId
+    Product.findById(prodId, (product) => {
+        if (product) {
+            res.render('admin/edit-product', {
+                pageTitle: 'Edit Product',
+                path: '/admin/edit-product',
+                editing: editMode,
+                product: product
+            });
+        } else {
+            return res.redirect('/')
+        }
+    })
 };
 
 exports.postAddProduct = (req, res, next) => {
