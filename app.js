81521fce0b73b039363f16f6fd068c1e3a4006e4
mohamednajiub <mohamednajiub@gmail.com>
2022-08-14 22:02:08 +0200

    feat: Add Cart

--- app.js
+++ app.js
@@ -61,6 +61,9 @@ sequelize.sync()
         }
         return user
     }).then(user => {
+        return user.createCart();
+    }).then(cart => {
+
         app.listen(3000);
     })
     .catch(error => {
