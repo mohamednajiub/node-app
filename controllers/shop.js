611c526d278fbce9acc4c1f2b302e85e37b6e658
mohamednajiub <mohamednajiub@gmail.com>
2022-08-14 22:56:45 +0200

    feat: Add Orders

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,5 +1,4 @@
 const Product = require('../models/product');
-const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
   Product.findAll()
@@ -121,15 +120,45 @@ exports.postCartDeleteProduct = (req, res, next) => {
 };
 
 exports.getOrders = (req, res, next) => {
-  res.render('shop/orders', {
-    path: '/orders',
-    pageTitle: 'Your Orders'
-  });
+  req.user.getOrders({ include: ['products'] })
+    .then(orders => {
+      res.render('shop/orders', {
+        path: '/orders',
+        pageTitle: 'Your Orders',
+        orders: orders
+      });
+    })
+    .catch(error => {
+      console.log(errors)
+    })
 };
 
-exports.getCheckout = (req, res, next) => {
-  res.render('shop/checkout', {
-    path: '/checkout',
-    pageTitle: 'Checkout'
-  });
+exports.postOrder = (req, res, next) => {
+  let fetchedCart;
+
+  req.user.getCart()
+    .then(cart => {
+      fetchedCart = cart;
+      return cart.getProducts()
+    }).then(products => {
+      console.log(products)
+      return req.user.createOrder()
+        .then(order => {
+          return order.addProducts(products.map(product => {
+            product.orderItem = {
+              quantity: product.cartItem.quantity
+            }
+            return product
+          }))
+        }).catch(error => {
+          console.log(errors)
+        })
+    }).then(result => {
+      return fetchedCart.setProducts(null)
+    }).then(result => {
+      res.redirect('/orders')
+    })
+    .catch(error => {
+      console.log(errors)
+    })
 };
