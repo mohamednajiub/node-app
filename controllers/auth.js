cf829db321d40be8345ef4cb9f3ca143494c3a76
mohamednajiub <mohamednajiub@gmail.com>
2022-08-17 23:00:14 +0200

    feat: Implement Cookies

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,37 @@
 exports.getLogin = (req, res, next) => {
-  res.render('auth/login', {
-    path: '/login',
-    pageTitle: 'Login'
+  const storedCookies = req.get('Cookie')
+
+  let isLoggedIn;
+
+  if (storedCookies) {
+
+    const isLoggedInCookie = storedCookies.split(';').find(cookie => {
+      return cookie.includes('loggedIn')
+    })
+
+    console.log('here')
+    isLoggedIn = isLoggedInCookie.split('=')[1]
+  }
+
+  if (isLoggedIn === 'true') {
+    console.log('here')
+    res.redirect(301, '/')
+  } else if (!isLoggedIn || isLoggedIn === 'false') {
+    res.render('auth/login', {
+      path: '/login',
+      pageTitle: 'Login',
+      isAuthenticated: isLoggedIn
+    });
+  }
+};
+
+exports.postLogin = (req, res, next) => {
+  req.isLoggedIn = true;
+  res.cookie('loggedIn', 'true', {
+    maxAge: 9000000,
+    httpOnly: true
   });
+  res.cookie('test1', 'true');
+  res.cookie('test2', 'true');
+  res.redirect('/');
 };
