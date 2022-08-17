991d7a8697677da1bf519ac81324d743bb985857
mohamednajiub <mohamednajiub@gmail.com>
2022-08-17 16:06:00 +0200

    feat: Add get products for shop

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,7 +1,7 @@
 const Product = require('../models/product');
 
 exports.getIndex = (req, res, next) => {
-  Product.fetchAll()
+  Product.find()
     .then(products => {
 
       res.render('shop/index', {
@@ -17,7 +17,7 @@ exports.getIndex = (req, res, next) => {
 
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll()
+  Product.find()
     .then(products => {
       res.render('shop/product-list', {
         prods: products,
