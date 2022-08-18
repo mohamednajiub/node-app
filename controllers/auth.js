b17d56855747092e12bc574cbbd77932ba4f4013
mohamednajiub <mohamednajiub@gmail.com>
2022-08-18 20:58:04 +0200

    fix: Error in signing up

--- controllers/auth.js
+++ controllers/auth.js
@@ -52,20 +52,21 @@ exports.postSignup = (req, res, next) => {
           return res.redirect('/signup')
         } else {
           return bcrypt.hash(password, 12)
+            .then(hashedPassword => {
+              const user = new User({
+                name,
+                email,
+                password: hashedPassword,
+                cart: { items: [] }
+              })
+              return user.save()
+            }).then(result => {
+              res.redirect('/login')
+            })
         }
       }
-    }).then(hashedPassword => {
-      const user = new User({
-        name,
-        email,
-        password: hashedPassword,
-        cart: { items: [] }
-      })
-      return user.save()
     })
-    .then(result => {
-      res.redirect('/login')
-    })
+
     .catch(error => {
       console.log(error)
     })
