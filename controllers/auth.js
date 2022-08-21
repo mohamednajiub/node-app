22acc9ab37ebe1d37576343bab26540a33d146ba
mohamednajiub <mohamednajiub@gmail.com>
2022-08-21 21:14:07 +0200

    feat: Add user input value

--- controllers/auth.js
+++ controllers/auth.js
@@ -44,7 +44,7 @@ exports.postLogin = (req, res, next) => {
     return res.render('auth/login', {
       path: '/login',
       pageTitle: 'Login',
-      errorMessage: errors.array()[0].msg
+      errorMessage: errors.array()[0].msg,
     });
   }
 
@@ -97,7 +97,13 @@ exports.getSignup = (req, res, next) => {
   res.render('auth/signup', {
     path: '/signup',
     pageTitle: 'Signup',
-    errorMessage: message
+    errorMessage: message,
+    oldInput: {
+      name: '',
+      email: '',
+      password: '',
+      passwordConfirmation: ''
+    }
   });
 };
 
@@ -114,7 +120,13 @@ exports.postSignup = (req, res, next) => {
     return res.status(422).render('auth/signup', {
       path: '/signup',
       pageTitle: 'Signup',
-      errorMessage: errors.array()[0].msg
+      errorMessage: errors.array()[0].msg,
+      oldInput: {
+        name: name,
+        email: email,
+        password: password,
+        passwordConfirmation: passwordConfirmation
+      }
     })
   }
 
